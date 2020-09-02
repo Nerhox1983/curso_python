bb586e0c5997ba4b8e9b03dd5c4036e9808ae33a
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-02 15:10:35 -0500

    Eliminar enumeracion en cuad.py

--- capitulo01/cuad.py
+++ capitulo01/cuad.py
@@ -1,15 +1,14 @@
-1 #!/usr/bin/env python
-2 #Un programa sencillo, para calcular cuadrados de números
-3
-4 def main():
-5 print "Se calcularán cuadrados de números"
-6
-7 n1 = input("Ingrese un número entero: ")
-8 n2 = input("Ingrese otro número entero: ")
-9
-10 for x in range(n1, n2):
-11 print x*x
-12
-13 print "Es todo por ahora"
-14
-15 main()
+#!/usr/bin/env python
+#Un programa sencillo, para calcular cuadrados de números
+def main():
+print "Se calcularán cuadrados de números"
+
+n1 = input("Ingrese un número entero: ")
+n2 = input("Ingrese otro número entero: ")
+
+for x in range(n1, n2):
+print x*x
+
+print "Es todo por ahora"
+
+main()
