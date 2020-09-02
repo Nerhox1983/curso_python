c559739f09123efc9e32d8e8a240f71499da07bd
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-02 16:29:59 -0500

    Ajustar linea #5 con Tabulacion en cuad.py

--- capitulo01/cuad.py
+++ capitulo01/cuad.py
@@ -2,7 +2,7 @@
 #Un programa sencillo, para calcular cuadrados de números
 def main():
     print ("Se calcularán cuadrados de números")
-n1 = int(input("Ingrese un número entero: "))
+    n1 = int(input("Ingrese un número entero: "))
 n2 = int(input("Ingrese otro número entero: "))
 
 for x in range(n1, n2):
