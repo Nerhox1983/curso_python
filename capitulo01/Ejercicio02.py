ee512db8dcdcf6e7827271e19f62bd112f36d435
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-02 17:28:32 -0500

    Crear linea#6 hasta linea#20 en Ejercicio02.py

--- capitulo01/Ejercicio02.py
+++ capitulo01/Ejercicio02.py
@@ -2,3 +2,19 @@
 #variable x en el cuerpo del ciclo for y después que se sale de tal ciclo. Volver a correr tres veces
 #el programa cuad con valores de entrada (3,5), (3,3) y (5,3) respectivamente, y explicar lo que
 #sucede.
+
+    auxiliar = 0
+
+    if n1>n2:
+        auxiliar = n2
+        n2 = n1
+        n1 = auxiliar
+        print ("n1=> ", n1)
+        print ("n2=> ", n2)
+        print ("auxiliar=> ", auxiliar)
+
+    for x in range(n1, n2):
+        print ("x", x)
+        print ("resultado: ", x*x)
+    
+    print ("Es todo por ahora")
