45dd1b78ca88212ba6da05c613fa96c7e25f8109
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-02 16:40:21 -0500

    Ajustar linea#8 con tabulacion en cuad.py

--- capitulo01/cuad.py
+++ capitulo01/cuad.py
@@ -5,7 +5,7 @@ def main():
     n1 = int(input("Ingrese un número entero: "))
     n2 = int(input("Ingrese otro número entero: "))
     for x in range(n1, n2):
-    print (x*x)
+        print (x*x)
 
 print ("Es todo por ahora")
 
