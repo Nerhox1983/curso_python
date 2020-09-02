30281d07f2cd4f3c5faa4144b64985dcc3254a28
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-02 17:14:37 -0500

    Crear Ejercicio02.py

---
+++ capitulo01/Ejercicio02.py
@@ -0,0 +1,4 @@
+#Ejercicio 1.2. Insertar instrucciones de depuración que permitan ver el valor asociado con la
+#variable x en el cuerpo del ciclo for y después que se sale de tal ciclo. Volver a correr tres veces
+#el programa cuad con valores de entrada (3,5), (3,3) y (5,3) respectivamente, y explicar lo que
+#sucede.
