5a2ca63ec56996c018827276fa36aab1b72248cb
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-03 09:19:11 -0500

    Eliminar entre líneas#5 y #8 en Ejercicio02.py

--- capitulo01/Ejercicio02.py
+++ capitulo01/Ejercicio02.py
@@ -1,9 +1,5 @@
 #Ejercicio 1.2. Insertar instrucciones de depuración que permitan ver el valor asociado con la
 #variable x en el cuerpo del ciclo for y después que se sale de tal ciclo. Volver a correr tres veces
 #el programa cuad con valores de entrada (3,5), (3,3) y (5,3) respectivamente, y explicar lo que
-#sucede.
-for x in range(n1, n2):
-    print ("x", x)
-    print ("resultado: ", x*x)
-    
+#sucede.    
 print ("Es todo por ahora")
