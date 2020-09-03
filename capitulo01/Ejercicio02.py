750b64ddf8120e13c74b7b76881b6eefa13334a5
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-03 09:06:45 -0500

    Ajustar Ejercicio02.py y OrdenadorNumeros.py

--- capitulo01/Ejercicio02.py
+++ capitulo01/Ejercicio02.py
@@ -3,15 +3,7 @@
 #el programa cuad con valores de entrada (3,5), (3,3) y (5,3) respectivamente, y explicar lo que
 #sucede.
 
-    auxiliar = 0
 
-    if n1>n2:
-        auxiliar = n2
-        n2 = n1
-        n1 = auxiliar
-        print ("n1=> ", n1)
-        print ("n2=> ", n2)
-        print ("auxiliar=> ", auxiliar)
 
     for x in range(n1, n2):
         print ("x", x)
---
+++ capitulo01/__pycache__/OrdenadorNumeros.py
@@ -0,0 +1,9 @@
+def OrdenarDosNumerosAscendentemente():
+        auxiliar = 0
+        if n1>n2:
+        auxiliar = n2
+        n2 = n1
+        n1 = auxiliar
+        print ("n1=> ", n1)
+        print ("n2=> ", n2)
+        print ("auxiliar=> ", auxiliar)
