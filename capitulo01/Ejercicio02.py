bd560a6be44a88d3fdc50765270de46e3aa7bee0
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-03 16:14:33 -0500

    Ajustar varios archivos

--- capitulo01/Ejercicio02.py
+++ capitulo01/Ejercicio02.py
@@ -1,5 +1,9 @@
 #Ejercicio 1.2. Insertar instrucciones de depuración que permitan ver el valor asociado con la
 #variable x en el cuerpo del ciclo for y después que se sale de tal ciclo. Volver a correr tres veces
 #el programa cuad con valores de entrada (3,5), (3,3) y (5,3) respectivamente, y explicar lo que
-#sucede.    
+#sucede.
+import ManejadorNumeros
+    
 print ("Es todo por ahora")
+
+ManejadorNumeros.OrdenarDosNumerosAscendentemente(4,2)
