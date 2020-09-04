12bff4a64ebc159a2486c34dd440fa9f4ddff80a
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 12:12:51 -0500

    Crear Reto0202.py en Capitulo02

---
+++ capitulo02/Reto0202.py
@@ -0,0 +1,21 @@
+#Ejercicio 2.1. Aplicando las reglas matemáticas de asociatividad, decidir cuáles de las siguientes
+#expresiones son iguales entre sí:
+import FuncionesNumeros02
+
+Numero10 = 10
+Numero100 = 100
+Numero1000 = 1000
+
+resultado1 = FuncionesNumeros02.CalcularReto0201PuntoA(a, b, c)
+resultado2 = FuncionesNumeros02.CalcularReto0201PuntoB(a, b, c)
+resultado3 = FuncionesNumeros02.CalcularReto0201PuntoC(a, b, c)
+resultado4 = FuncionesNumeros02.CalcularReto0201PuntoD(a, b, c)
+resultado5 = FuncionesNumeros02.CalcularReto0201PuntoE(b)
+resultado6 = FuncionesNumeros02.CalcularReto0201PuntoF(b)
+
+print ("Resultado1=> ", resultado1)
+print ("Resultado2=> ", resultado2)
+print ("Resultado3=> ", resultado3)
+print ("Resultado4=> ", resultado4)
+print ("Resultado5=> ", resultado5)
+print ("Resultado6=> ", resultado6)
