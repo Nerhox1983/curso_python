e4bd40d18479d244158ca3fa061d9181f7b3b784
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 12:18:45 -0500

    Ajustar línea#10 en Reto0202.py

--- capitulo02/Reto0202.py
+++ capitulo02/Reto0202.py
@@ -7,7 +7,7 @@ Numero100 = 100
 Numero1000 = 1000
 
 resultado1 = FuncionesNumeros02.CalcularReto0201PuntoA(Numero10, Numero100, Numero1000)
-resultado2 = FuncionesNumeros02.CalcularReto0201PuntoB(a, b, c)
+resultado2 = FuncionesNumeros02.CalcularReto0201PuntoB(Numero10, Numero100, Numero1000)
 resultado3 = FuncionesNumeros02.CalcularReto0201PuntoC(a, b, c)
 resultado4 = FuncionesNumeros02.CalcularReto0201PuntoD(a, b, c)
 resultado5 = FuncionesNumeros02.CalcularReto0201PuntoE(b)
