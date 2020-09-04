38c948aa056a185afa3968abdc148a670d20a49e
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 11:29:25 -0500

    Crear FuncionesNumeros02.py Ajustar linea#9 y #15 en Reto0201.py

---
+++ capitulo02/FuncionesNumeros02.py
@@ -0,0 +1,15 @@
+def OrdenarDosNumerosAscendentemente(numero1, numero2):
+    auxiliar = 0
+    if numero1>numero2:
+        auxiliar = numero2
+        numero2 = numero1
+        numero1 = auxiliar
+        print ("numero1=> ", numero1)
+        print ("numero2=> ", numero2)
+        print ("auxiliar=> ", auxiliar)
+
+def CalcularReto0201PuntoA(a, b, c):
+    return ((b * b) - (4 * a * c)) / (2 * a),
+
+def CalcularReto0201PuntoB(a, b, c):
+    return (b * b - 4 * a * c) / (2 * a),
--- capitulo02/Reto0201.py
+++ capitulo02/Reto0201.py
@@ -1,18 +1,19 @@
 #Ejercicio 2.1. Aplicando las reglas matemáticas de asociatividad, decidir cuáles de las siguientes
 #expresiones son iguales entre sí:
-import ManejadorNumeros
+import FuncionesNumeros02
 
 a = 10
 b = 100
 c = 1000
 
-resultado2 = (b * b - 4 * a * c) / (2 * a),
+resultado1 = FuncionesNumeros02.CalcularReto0201PuntoA(a, b, c)
+resultado2 = 
 resultado3 = b * b - 4 * a * c / 2 * a,
 resultado4 =  (b * b) - (4 * a * c / 2 * a)
 resultado5 =  1 / 2 * b
 resultado6 =  b / 2.
 
-#print ("Resultado1=> ", resultado1)#
+print ("Resultado1=> ", resultado1)#
 print ("Resultado2=> ", resultado2)
 print ("Resultado3=> ", resultado3)
 print ("Resultado4=> ", resultado4)
