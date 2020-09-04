55f48a5d18650db97570c037d9c27e31f44bf1b6
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 17:46:41 -0500

    Crear Reto0206.py

---
+++ capitulo02/Reto0506.py
@@ -0,0 +1,5 @@
+import FuncionesNumeros02
+
+for miMejorAmigoActual in range (FuncionesNumeros02.Numero1, FuncionesNumeros02.Numero5):
+    miMejorAmigo = input ("Dame el nombre de uno de tus mejores amigos: ")
+    print ("Tu mejor amigo se llama: ", miMejorAmigo)
