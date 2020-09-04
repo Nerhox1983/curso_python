b49f7f451aea3e654b82330c2096ce0e4c9542e7
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 17:51:43 -0500

    Crear línea#5 en Reto0206.py

--- capitulo02/Reto0206.py
+++ capitulo02/Reto0206.py
@@ -2,3 +2,4 @@ import FuncionesNumeros02
 
 for miMejorAmigoActual in range (FuncionesNumeros02.Numero1, FuncionesNumeros02.Numero5):
     miMejorAmigo = input ("Dame el nombre de uno de tus mejores amigos: ")
+    print ("Hola ", miMejorAmigo)
