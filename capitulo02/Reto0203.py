487fb92f564b753862c2697b2c841a4a8d0686ea
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-09-04 13:11:50 -0500

    Crear Reto0203.py y crear Reto0204.py

---
+++ capitulo02/Reto0203.py
@@ -0,0 +1,21 @@
+#Ejercicio 2.1. Aplicando las reglas matemáticas de asociatividad, decidir cuáles de las siguientes
+#expresiones son iguales entre sí:
+import FuncionesNumeros02
+
+Numero10 = 10.0
+Numero100 = 100.0
+Numero1000 = 1000.0
+
+resultado1 = FuncionesNumeros02.CalcularReto0201PuntoA(Numero10, Numero100, Numero1000)
+resultado2 = FuncionesNumeros02.CalcularReto0201PuntoB(Numero10, Numero100, Numero1000)
+resultado3 = FuncionesNumeros02.CalcularReto0201PuntoC(Numero10, Numero100, Numero1000)
+resultado4 = FuncionesNumeros02.CalcularReto0201PuntoD(Numero10, Numero100, Numero1000)
+resultado5 = FuncionesNumeros02.CalcularReto0201PuntoE(Numero100)
+resultado6 = FuncionesNumeros02.CalcularReto0201PuntoF(Numero100)
+
+print ("Resultado1=> ", resultado1)
+print ("Resultado2=> ", resultado2)
+print ("Resultado3=> ", resultado3)
+print ("Resultado4=> ", resultado4)
+print ("Resultado5=> ", resultado5)
+print ("Resultado6=> ", resultado6)
---
+++ capitulo02/Reto0204.py
@@ -0,0 +1,5 @@
+#Ejercicio 2.4. Escribir un ciclo definido para imprimir por pantalla todos los números entre 10
+#y 20.
+
+for numeroActual in range (10,21):
+    print (numeroActual)
